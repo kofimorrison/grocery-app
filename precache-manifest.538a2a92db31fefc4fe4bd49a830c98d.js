c2c22e63132badc56c1c984bacc8f61b7006201d
kofimorrison <49091928+kofimorrison@users.noreply.github.com>
2019-05-08 00:56:51 +0200

    Updates

---
+++ precache-manifest.538a2a92db31fefc4fe4bd49a830c98d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4b6a733fe685e51d32f38b104a3d6ea7",
+    "url": "/groceryapp/index.html"
+  },
+  {
+    "revision": "a4d50a2a6d0a3584d762",
+    "url": "/groceryapp/static/css/2.ad9eb4b5.chunk.css"
+  },
+  {
+    "revision": "a4d50a2a6d0a3584d762",
+    "url": "/groceryapp/static/js/2.b0f4c8b7.chunk.js"
+  },
+  {
+    "revision": "24580d98d0eb36986293",
+    "url": "/groceryapp/static/js/main.1f92688a.chunk.js"
+  },
+  {
+    "revision": "afe39e306da58f37b211",
+    "url": "/groceryapp/static/js/runtime~main.50675701.js"
+  }
+]);
